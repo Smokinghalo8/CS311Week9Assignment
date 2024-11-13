f3e2ebd211d13d252a45199987a55561c594a44b
Smokinghalo8 <ethanfbuckner@gmail.com>
2024-11-13 11:04:35 -0500

    use client

--- app/videos/video/add/page.jsx
+++ app/videos/video/add/page.jsx
@@ -1,32 +1,68 @@
+"use client" //added this
+
+import { useState } from 'react'
 import { PrismaClient } from '@prisma/client'
 import { redirect } from 'next/navigation'
 
 const prisma = new PrismaClient()
 
-const AddVideoPage = async () => {
+const AddVideoPage = () => {
+  const [name, setName] = useState('')
+  const [url, setUrl] = useState('')
+  const [votes, setVotes] = useState(0)
+  const [length, setLength] = useState(0)
+
   const handleSubmit = async (event) => {
     event.preventDefault()
-    const formData = new FormData(event.target)
-    const name = formData.get('name')
-    const url = formData.get('url')
-    const votes = parseInt(formData.get('votes'), 10)
-    const length = parseInt(formData.get('length'), 10)
 
     await prisma.video.create({
       data: { name, url, votes, length },
     })
 
+    // Redirect to the list of videos
     redirect('/videos')
   }
-
+//changed out the return function
   return (
     <div>
       <h1>Add New Video</h1>
       <form onSubmit={handleSubmit}>
-        <input name="name" required />
-        <input name="url" required />
-        <input name="votes" type="number" required />
-        <input name="length" type="number" required />
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          URL:
+          <input
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Votes:
+          <input
+            type="number"
+            value={votes}
+            onChange={(e) => setVotes(Number(e.target.value))}
+            required
+          />
+        </label>
+        <label>
+          Length (seconds):
+          <input
+            type="number"
+            value={length}
+            onChange={(e) => setLength(Number(e.target.value))}
+            required
+          />
+        </label>
         <button type="submit">Add Video</button>
       </form>
     </div>
